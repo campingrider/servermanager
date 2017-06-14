55d897e29c3f6a67d6c5cf2847b0ee4548fe6cc4
Camping_RIDER <janosch.zoller@gmx.de>
2017-06-14 03:28:08 +0200

    Set phpdoc to work with config file

--- classes/manager.php
+++ classes/manager.php
@@ -7,12 +7,12 @@
 	 */
 	 
 	class manager {
-		private static $settings_descriptions = new Array(
+		private static $settings_descriptions = array(
 			'title'=>'The following phrase is shown as a title for the server manager',
 			'banner_path'=>'The following path points to the banner image'
 		);
 		
-		private $settings_general = new Array(
+		private $settings_general = array(
 			'title'=>'',
 			'banner_path'=>''
 		);
