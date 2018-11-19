d47396ddb5de723e04e8d163fe9655a444c97e7b
Camping_RIDER <janosch.zoller@gmx.de>
2018-11-19 20:00:16 +0100

    Adopt PSR-12 coding styleguide

--- classes/manager.php
+++ classes/manager.php
@@ -1,69 +1,101 @@
 <?php
-	namespace campingrider\servermanager;
-	
-	/** This class contains overall settings and controls the behavior of the system. 
-	 * 
-	 * @package servermanager
-	 */	 
-	class manager {
-		
-		/** @var string[] description for all settings which can be written out to an ini file */
-		private static $settings_descriptions = array(
-			'title'=>'The following phrase is shown as a title for the server manager',
-			'banner_path'=>'The following path points to the banner image'
-		);
-		
-		/** @var string[] contains default values for all settings */
-		private $settings = array(
-			'title'=>'Title not configured yet',
-			'banner_path'=>'no_path.png'
-		);
-		
-		/** Constructor.
-		 * 
-		 * @param string|string[] $settings settings to load, either directly via array or through the path of an ini file containing the information 
-		 */
-		public function __construct($settings) {
-			
-			$loaded = array();
-			
-			// load either from file or from array
-			if (is_string($settings)) {
-				if (is_file($settings)) {
-					$loaded = parse_ini_file($settings);
-				}
-			} else if (is_array($settings)) {
-				$loaded = $settings;
-			}
-			
-			// overwrite default settings with loaded settings
-			$write_ini = false;
-			foreach ($this->settings as $setting => $defaultvalue) {
-				if (isset($loaded[$setting])) {
-					$this->settings[$setting] = $loaded[$setting];
-				} else {
-					// setting was not found in loaded values - if loaded from ini file set to rewrite file
-					$write_ini = write_ini OR (is_string($settings) AND is_dir(dirname($settings)));
-				}
-			}
-			
-			// write ini with newer version if not complete
-			if ($write_ini) {
-				$content = "; General settings. Adjust to your needs.\n; ------------------------------ \n\n";
-				foreach ($this->settings as $setting => $value) {
-					if (isset(manager::$settings_descriptions[$setting])) $content .= "; ".manager::$settings_descriptions[$setting]."\n";
-					$content .= $setting.' = "'.$value.'"'."\n\n"; 
-				}
-				file_put_contents($settings,$content);
-			}			
-		}
-		
-		/** Getter for the title of the manager.
-		 * 
-		 * @return string title of the manager
-		 */
-		public function get_title() {
-			return $this->settings['title'];
-		}
-	}
-?>
+/**
+ * Central class managing all features and content
+ *
+ * Cares for managing all servers and services and displays them to the user.
+ *
+ * @package campingrider\servermanager
+ */
+
+namespace campingrider\servermanager;
+
+/**
+ * This class contains overall settings and controls the behavior of the system.
+ *
+ * @package campingrider\servermanager
+ */
+class Manager
+{
+
+    /**
+     * Contains description for all settings which can be written out to an ini file.
+     *
+     * @var string[] $settings_descriptions
+     */
+    private static $settings_descriptions = array(
+        'title'       => 'The following phrase is shown as a title for the server manager',
+        'banner_path' => 'The following path points to the banner image',
+    );
+
+    /**
+     * Contains default values for all settings.
+     *
+     * @var string[] $settings
+     */
+    private $settings = array(
+        'title'       => 'Title not configured yet',
+        'banner_path' => 'no_path.png',
+    );
+
+    /**
+     * Constructor.
+     *
+     * @param string|string[] $settings Either string as Path of an ini-File containing all settings which shall
+     *                                  be loaded or Array of strings containing all settings which shall be loaded.
+     * @throws RuntimeException Thrown if the ini-File is not found at the given location.
+     * @throws InvalidArgumentException Thrown if $settings is not of type string or array.
+     */
+    public function __construct($settings)
+    {
+
+        $loaded_settings = array();
+
+        // load either from file or from array.
+        if (is_string($settings)) {
+            if (is_file($settings)) {
+                $loaded_settings = parse_ini_file($settings);
+            } else {
+                throw new RuntimeException("File $settings not found!");
+            }
+        } elseif (is_array($settings)) {
+            $loaded_settings = $settings;
+        } else {
+            throw new InvalidArgumentException(
+                '$settings needs to be of type string or string[], not ' . gettype($settings) . '!'
+            );
+        }
+
+        // overwrite default settings with loaded settings.
+        $write_ini = false;
+        foreach ($this->settings as $setting_id => $defaultvalue) {
+            if (isset($loaded_settings[ $setting_id ])) {
+                $this->settings[ $setting_id ] = $loaded_settings[ $setting_id ];
+            } else {
+                // setting was not found in loaded values - if loaded from ini file set to rewrite file!
+                $write_ini = write_ini || (is_string($settings) && is_dir(dirname($settings)));
+            }
+        }
+
+        // write ini with newer version if ini was not complete.
+        if ($write_ini) {
+            $content = "; General settings. Adjust to your needs.\n; ------------------------------ \n\n";
+            foreach ($this->settings as $setting_id => $value) {
+                if (isset(manager::$settings_descriptions[ $setting_id ])) {
+                    $content .= '; ' . manager::$settings_descriptions[ $setting_id ] . "\n";
+                }
+                $content .= $setting_id . ' = "' . $value . '"' . "\n\n";
+            }
+            file_put_contents($settings, $content);
+        }
+    }
+
+    /**
+     * Getter for the title of the manager.
+     *
+     * @return string title of the manager
+     */
+    public function getTitle()
+    {
+        return $this->settings['title'];
+    }
+}
--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <?php
-	namespace campingrider\servermanager;
-	
-	spl_autoload_register(function ($name) {
-		if (strpos($name,__NAMESPACE__) === 0) {
-			$classpath = './classes/'.str_replace('\\',DIRECTORY_SEPARATOR,str_replace(__NAMESPACE__.'\\','',$name)).'.php';
-			if (is_file($classpath)) {
-				include_once($classpath);
-			} else {
-				die('File for required class '.$name.' was searched for at '.$classpath.' but could not be found.');
-			}
-		}
-	});
-	
-	$manager = new manager("./custom/settings.ini");
-?>
+    namespace campingrider\servermanager;
+    
+    spl_autoload_register(function ($name) {
+        if (strpos($name, __NAMESPACE__) === 0) {
+            $classpath = './classes/';
+            $classpath .= str_replace('\\', DIRECTORY_SEPARATOR, str_replace(__NAMESPACE__ . '\\', '', $name)) . '.php';
+            if (is_file($classpath)) {
+                include_once($classpath);
+            } else {
+                die(
+                    'File for required class '
+                    . $name .
+                    ' was searched for at '
+                    . $classpath .
+                    ' but could not be found.'
+                );
+            }
+        }
+    });
+    
+    $manager = new Manager("./custom/settings.ini");
+    ?>
+
 <!DOCTYPE html>
 <html lang="de">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-		<title><?php echo $manager->get_title(); ?> - RIDERs Server Manager</title>
-		<!-- 
-		This software uses icons from Font-Awesome, http://fontawesome.io, released under SIL OFL 1.1.
-		The svg files used were created from font-awesome by Font-Awesome-SVG-PNG, https://github.com/encharm/Font-Awesome-SVG-PNG, released under MIT license. 
-		-->
-	</head>
-	<body>
-		<header>
-			<h1><?php echo $manager->get_title(); ?></h1>
-		</header>
-	</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <title><?php echo $manager->getTitle(); ?> - RIDERs Server Manager</title>
+        <!-- 
+        This software uses icons from Font-Awesome, http://fontawesome.io, released under SIL OFL 1.1.
+        The svg files used were created from font-awesome by Font-Awesome-SVG-PNG, 
+        https://github.com/encharm/Font-Awesome-SVG-PNG, released under MIT license. 
+        -->
+    </head>
+    <body>
+        <header>
+            <h1><?php echo $manager->getTitle(); ?></h1>
+        </header>
+    </body>
 </html>
