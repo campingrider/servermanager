0418ee778ce006d7804d8c44d5b3e187caa14efc
Camping_RIDER <janosch.zoller@gmx.de>
2019-08-10 22:27:03 +0200

    ref(css): adjust server layout

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
         <link rel="stylesheet" href="css/layout.css" type="text/css">
+        <link rel="stylesheet" href="css/servers.css" type="text/css">
         <title><?php echo $manager->getTitle(); ?> - RIDERs Server Manager</title>
         <!-- 
         This software uses icons from Font-Awesome, http://fontawesome.io, released under SIL OFL 1.1.
